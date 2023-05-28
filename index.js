296b9fa6228f4d8c6b3452c744e174fd889b8e78
Tyler <tyler.le@edu.sait.ca>
2023-05-28 13:29:32 -0600

    Fixes file layout

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector('canvas')
-
+const c = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
-console.log(canvas)
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
