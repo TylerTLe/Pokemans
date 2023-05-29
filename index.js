4a25edde05623e8f4349ce234d4852a3fc432fa3
Tyler <tyler.le@edu.sait.ca>
2023-05-29 17:49:28 -0600

    Adds player movement

--- index.js
+++ index.js
@@ -14,8 +14,43 @@ image.src = './Images/Pellet Town.png'
 const playerImage = new Image()
 playerImage.src = './Images/playerDown.png'
 
-image.onload = () => {
-    c.drawImage(image, -810, -600)
+class Sprite {
+    constructor({posistion, velocity, image}) {
+        this.posistion = posistion
+        this.image = image
+    }
+
+    draw() {
+        c.drawImage(this.image, this.posistion.x, this.posistion.y)
+    }
+}
+
+const background = new Sprite({
+    posistion: {
+        x: -810,
+        y: -600
+    },
+    image: image
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+function animation() {
+    window.requestAnimationFrame(animation)
+    background.draw()
     c.drawImage(
         playerImage,
         0,
@@ -25,5 +60,47 @@ image.onload = () => {
         canvas.width / 2 - playerImage.width / 2,
         canvas.height / 2 - playerImage.height / 2,
         playerImage.width / 4,
-        playerImage.height,)
+        playerImage.height,
+    )
+
+    if (keys.w.pressed) background.posistion.y = background.posistion.y + 3
+    else if (keys.a.pressed) background.posistion.x = background.posistion.x + 3
+    else if (keys.s.pressed) background.posistion.y = background.posistion.y - 3
+    else if (keys.d.pressed) background.posistion.x = background.posistion.x - 3
+    
 }
+animation()
+
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'w':
+        keys.w.pressed = true
+            break
+        case 'a':
+        keys.a.pressed = true
+            break
+        case 's':
+        keys.s.pressed = true
+            break
+        case 'd':
+        keys.d.pressed = true
+            break
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w':
+        keys.w.pressed = false
+            break
+        case 'a':
+        keys.a.pressed = false
+            break
+        case 's':
+        keys.s.pressed = false
+            break
+        case 'd':
+        keys.d.pressed = false
+            break
+    }
+})
