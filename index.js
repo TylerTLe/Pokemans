e7c4a67d9dfb4535697fe18e33a7af6c1daa22ee
Tyler <tyler.le@edu.sait.ca>
2023-05-29 17:24:01 -0600

    Creates and renders character

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ canvas.height = 576
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+// Map Image
 const image = new Image()
 image.src = './Images/Pellet Town.png'
+// Player Image
+const playerImage = new Image()
+playerImage.src = './Images/playerDown.png'
 
 image.onload = () => {
-    c.drawImage(image, -750, -550)
+    c.drawImage(image, -810, -600)
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - playerImage.width / 2,
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height,)
 }
