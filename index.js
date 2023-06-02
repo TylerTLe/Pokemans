1cb7e39624ccf4bf1c3fbb1a0f66f0dff231efec
Tyler <tyler.le@edu.sait.ca>
2023-06-02 12:52:21 -0600

    Maps out collkision boxes

--- index.js
+++ index.js
@@ -56,6 +56,11 @@ class Sprite {
     this.position = position;
     this.image = image;
     this.frames = frames;
+
+    this.image.onload = () => {
+      this.width = this.image.width / this.frames.max
+      this.height = this.image.height
+    }
   }
 
   draw() {
@@ -107,23 +112,34 @@ const keys = {
   },
 };
 
-const testBoundary = new Boundary({
-  position: {
-    x: 400,
-    y: 400,
-  },
-});
+const moveables = [background, ...boundaries];
 
-const moveables = [background, testBoundary];
+function rectangularCollision({rectangle1, rectangle2}) {
+  return (
+    rectangle1.position.x + rectangle1.width >= rectangle2.position.x && 
+    rectangle1.position.x <= rectangle2.position.x + rectangle2.width && 
+    rectangle1.position.y <= rectangle2.position.y + rectangle2.height && 
+    rectangle1.position.y + rectangle1.height >= rectangle2.position.y
+  )
+}
 
 function animation() {
-  window.requestAnimationFrame(animation);
-  background.draw();
-  // boundaries.forEach(Boundary =>  {
-  //     Boundary.draw()
-  // })
-  testBoundary.draw();
+  window.requestAnimationFrame(animation)
+  background.draw()
+  boundaries.forEach((Boundary) => {
+    Boundary.draw()  
+
+    if (
+      rectangularCollision({
+        rectangle1: player,
+        rectangle2: Boundary
+      })
+    ) {
+      console.log('collision')
+    }
+  })
   player.draw();
+
 
   if (keys.w.pressed && lastKey === "w") {
     moveables.forEach((moveable) => {
