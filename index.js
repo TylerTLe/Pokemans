0bd0ca15f9baa4354f16b64147ec7380438563a0
Tyler <tyler.le@edu.sait.ca>
2023-06-04 13:15:18 -0600

    Adds foreground objects and class file

--- index.js
+++ index.js
@@ -9,21 +9,6 @@ for (let i = 0; i < collisions.length; i += 70) {
   collisionsMap.push(collisions.slice(i, 70 + i));
 }
 
-class Boundary {
-  static height = 48;
-  static width = 48;
-  constructor({ position }) {
-    this.position = position;
-    this.height = 48;
-    this.width = 48;
-  }
-
-  draw() {
-    c.fillStyle = "rgba(255,0,0,0)";
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-}
-
 const boundaries = [];
 offset = {
   x: -739,
@@ -42,41 +27,19 @@ collisionsMap.forEach((row, i) => {
         })
       );
   });
-});
+})
 
 // Map Image
 const image = new Image();
 image.src = "./Images/Pellet Town.png";
+
+// foreground Image
+const foregroundImage = new Image();
+foregroundImage.src = './Images/foregroundobj.png';
+
 // Player Image
 const playerImage = new Image();
 playerImage.src = "./Images/playerDown.png";
-
-class Sprite {
-  constructor({ position, velocity, image, frames = { max: 1 } }) {
-    this.position = position;
-    this.image = image;
-    this.frames = frames;
-
-    this.image.onload = () => {
-      this.width = this.image.width / this.frames.max
-      this.height = this.image.height
-    }
-  }
-
-  draw() {
-    c.drawImage(
-      this.image,
-      0,
-      0,
-      this.image.width / this.frames.max,
-      this.image.height,
-      this.position.x,
-      this.position.y,
-      this.image.width / this.frames.max,
-      this.image.height
-    );
-  }
-}
 
 const player = new Sprite({
   position: {
@@ -97,6 +60,14 @@ const background = new Sprite({
   image: image,
 });
 
+const foreground = new Sprite({
+  position: {
+    x: offset.x,
+    y: offset.y,
+  },
+  image: foregroundImage,
+});
+
 const keys = {
   w: {
     pressed: false,
@@ -112,7 +83,7 @@ const keys = {
   },
 };
 
-const moveables = [background, ...boundaries];
+const moveables = [background, ...boundaries, foreground];
 
 function rectangularCollision({rectangle1, rectangle2}) {
   return (
@@ -130,6 +101,7 @@ function animation() {
     Boundary.draw()  
   })
   player.draw();
+  foreground.draw();
 
   let moving = true
   if (keys.w.pressed && lastKey === 'w') {
