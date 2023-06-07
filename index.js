f8d143e9506074602483d61263a999e8c194a12a
Tyler <tyler.le@edu.sait.ca>
2023-06-07 13:58:13 -0600

    Fixes battle

--- index.js
+++ index.js
@@ -300,7 +300,7 @@ function animation() {
   }
 }
 
-// animation();
+animation();
 
 const battleBackgroundImage = new Image()
 battleBackgroundImage.src = './Images/battleBackground.png'
@@ -348,8 +348,6 @@ function animateBattle() {
   draggle.draw()
   emby.draw()
 }
-// For testing
-animateBattle()
 
 let lastKey = "";
 window.addEventListener("keydown", (e) => {
